e0db78a4d9c59c4870a1ec113338cf951a0b9873
Muhamed Hasanbasic <134994729+Muhaq43@users.noreply.github.com>
2023-06-20 02:41:41 +0200

    Add files via upload

--- Schreibtisch/Project/proc_local.c
+++ Schreibtisch/Project/proc_local.c
@@ -1,58 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>      // Standard-Ein-/Ausgabe-Funktionen
+#include <stdlib.h>     // Allgemeine Funktionen (z.B. Speicherallokation)
+#include <unistd.h>     // POSIX-Betriebssystem-API
 
-static
-void print_pid_files(char *filename, char *header, int max_lines, FILE *fpo) {
+// Funktion zum Drucken von PID-Dateien
+static void print_pid_files(char *filename, char *header, int max_lines, FILE *fpo) {
     char buffer[1001];
     int n;
     FILE *fp;
-    if (max_lines == 0) return;
+    
+    if (max_lines == 0) return; // Abbruch, wenn max_lines 0 ist
+    
     fp = fopen(filename, "r");
     if (!fp) {
-        perror("open status failed");
+        perror("Fehler beim Öffnen der Statusdatei"); // Fehlermeldung, falls Statusdatei nicht geöffnet werden kann
         exit(-1);
     }
+    
     fprintf(stdout, "%s\n", header);
     fprintf(stdout, "---------------------------------------------------------\n");
     fprintf(fpo, "%s\n", header);
     fprintf(fpo, "---------------------------------------------------------\n");
+    
     n = 0;
     while(feof(fp) == 0) {
-        fgets(buffer,1000,fp);
-        fprintf(stdout, "%s\n", buffer);
-        fprintf(fpo, "%s\n", buffer);
-	n++;
-	if (max_lines > 0 && n == max_lines) break;
+        fgets(buffer, 1000, fp); // Zeile aus Datei lesen
+        fprintf(stdout, "%s\n", buffer); // Zeile auf der Konsole ausgeben
+        fprintf(fpo, "%s\n", buffer); // Zeile in der Ausgabedatei speichern
+        n++;
+        if (max_lines > 0 && n == max_lines) break; // Abbruch nach max_lines Zeilen
     }
+    
     fclose(fp);
 }
 
 int main(int argc, char *argv[]) {
-    int pid = getpid();
+    int pid = getpid(); // Aktuelle Prozess-ID
     char filename[100];
     char output[100];
-    sprintf(output, "%s", "./project_out.txt");
+    sprintf(output, "%s", "./project_out.txt"); // Ausgabedatei festlegen
     FILE *fpo = fopen(output, "a");
     if (!fpo) {
-        perror("open output file failed");
+        perror("Fehler beim Öffnen der Ausgabedatei"); // Fehlermeldung, falls Ausgabedatei nicht geöffnet werden kann
         exit(-1);
     }
-    sprintf(filename, "/proc/%d/status", pid);
-    print_pid_files(filename, "status", -1, fpo);
-
-    sprintf(filename, "/proc/%d/maps", pid);
-    print_pid_files(filename, "maps", -1, fpo);
+    
+    sprintf(filename, "/proc/%d/status", pid); // Pfad zur Statusdatei des aktuellen Prozesses erstellen
+    print_pid_files(filename, "status", -1, fpo); // Statusdatei ausgeben (-1 für alle Zeilen)
+    
+    sprintf(filename, "/proc/%d/maps", pid); // Pfad zur Maps-Datei des aktuellen Prozesses erstellen
+    print_pid_files(filename, "maps", -1, fpo); // Maps-Datei ausgeben (-1 für alle Zeilen)
         
-    sprintf(filename, "/proc/%d/statm", pid);
-    print_pid_files(filename, "statm", 1, fpo);
-
+    sprintf(filename, "/proc/%d/statm", pid); // Pfad zur Statm-Datei des aktuellen Prozesses erstellen
+    print_pid_files(filename, "statm", 1, fpo); // Statm-Datei ausgeben (nur eine Zeile)
+    
     fprintf(stdout, "---------------------------------------------------------\n");
     fprintf(stdout, "---------------------------------------------------------\n\n");
     fprintf(fpo, "---------------------------------------------------------\n");
     fprintf(fpo, "---------------------------------------------------------\n\n");
     fclose(fpo);
 
-    sleep(20);
+    sleep(20); // 20 Sekunden warten
     exit(0);
 }
--- Schreibtisch/Project/project.c
+++ Schreibtisch/Project/project.c
@@ -1,149 +1,150 @@
+#include <stdio.h>      // Standard-Ein-/Ausgabe-Funktionen
+#include <stdlib.h>     // Allgemeine Funktionen (z.B. Speicherallokation)
+#include <unistd.h>     // POSIX-Betriebssystem-API
+#include <signal.h>     // Signal-Handling-Funktionen
+#include <sys/types.h>  // Systemdatentypen (z.B. pid_t)
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-
+// Funktion zum Erzeugen eines Kindprozesses
 static int do_fork() {
     int pid;
 
-    if ((pid = fork()) < 0) {
-        perror("fork failed");
+    if ((pid = fork()) < 0) { // Forken des aktuellen Prozesses
+        perror("Fehler bei fork"); // Fehlermeldung, falls Fork fehlschlägt
         return 2;
     }
-    if (pid == 0) {
-        // Redirect stdout to the file
+    if (pid == 0) { // Kindprozess
+        // Stdout auf die Datei umleiten
         freopen("./project_out.txt", "a", stdout);
 
+        // Programm 'proc_local' ausführen
         execlp("./proc_local", "proc_local", (char *) 0);
-        perror("proc_local failed");
+        perror("proc_local fehlgeschlagen"); // Fehlermeldung, falls Ausführung fehlschlägt
         return 3;
     }
-    return pid;
+    return pid; // Rückgabe der PID des Kindprozesses
 }
 
+// Funktion zum Drucken des Inhalts einer Datei
 void printFileContents(const char* filename) {
     FILE* file = fopen(filename, "r");
     if (!file) {
-        perror("open file failed");
+        perror("Fehler beim Öffnen der Datei"); // Fehlermeldung, falls Datei nicht geöffnet werden kann
         return;
     }
 
-    printf("Contents of %s:\n", filename);
+    printf("Inhalt von %s:\n", filename);
     printf("--------------------\n");
 
     char buffer[1024];
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        printf("%s", buffer);
+        printf("%s", buffer); // Zeile aus Datei drucken
     }
 
     fclose(file);
 }
 
-// Function to kill all child processes
+// Funktion zum Beenden aller Kindprozesse
 void killAllChildProcesses() {
-    pid_t myPid = getpid();
-    pid_t pgrp = getpgrp();
+    pid_t myPid = getpid(); // PID des aktuellen Prozesses
+    pid_t pgrp = getpgrp(); // Gruppen-ID des aktuellen Prozesses
 
-    // Create an array to store child process IDs
+    // Array zum Speichern der Kindprozess-IDs erstellen
     pid_t childPids[100];
     int childCount = 0;
 
-    // Find all child process IDs
+    // Alle Kindprozess-IDs finden
     pid_t pid;
     for (pid = 1; pid < myPid; pid++) {
-        if (getpgid(pid) == pgrp) {
-            childPids[childCount++] = pid;
+        if (getpgid(pid) == pgrp) { // Prüfen, ob Prozess zur gleichen Gruppe gehört
+            childPids[childCount++] = pid; // Kindprozess-ID speichern
         }
     }
 
-    // Terminate each child process
+    // Jeden Kindprozess beenden
     int i;
     for (i = 0; i < childCount; i++) {
-        if (kill(childPids[i], SIGTERM) == -1) {
-            perror("Error killing child process");
+        if (kill(childPids[i], SIGTERM) == -1) { // Kindprozess mit SIGTERM beenden
+            perror("Fehler beim Beenden des Kindprozesses"); // Fehlermeldung, falls Beenden fehlschlägt
         }
     }
 
-    printf("All child processes killed.\n");
+    printf("Alle Kindprozesse beendet.\n");
 }
-
 
 int main() {
     int n_process = 0;
     int i;
     int ret;
     char output[100];
-    sprintf(output, "%s", "./project_out.txt");
+    sprintf(output, "%s", "./project_out.txt"); // Ausgabedatei festlegen
     FILE *fpo = fopen(output, "w");
     if (!fpo) {
-        perror("open output file failed");
+        perror("Fehler beim Öffnen der Ausgabedatei"); // Fehlermeldung, falls Ausgabedatei nicht geöffnet werden kann
         exit(-1);
     }
     ret = 0;
 
-    fprintf(fpo, "Process Generation and Branching:\n");
+    fprintf(fpo, "Prozesserzeugung und Verzweigung:\n");
 
-    // Menu loop
+    // Menüschleife
     while (1) {
-        printf("1. Create processes\n");
-        printf("2. Read project_out.txt\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
+        printf("1. Prozesse erstellen\n");
+        printf("2. project_out.txt lesen\n");
+        printf("3. Beenden\n");
+        printf("Geben Sie Ihre Wahl ein: ");
         int choice;
-        scanf("%d", &choice);
+        scanf("%d", &choice); // Benutzereingabe auslesen
 
         switch (choice) {
             case 1: {
-                // Create processes
+                // Prozesse erstellen
                 if (n_process > 0) {
-                    printf("Processes already created.\n");
+                    printf("Prozesse bereits erstellt.\n"); // Fehlermeldung, falls Prozesse bereits erstellt wurden
                 } else {
-                    printf("Enter the number of processes to create (greater than 0): ");
-                    scanf("%d", &n_process);
+                    printf("Geben Sie die Anzahl der zu erstellenden Prozesse ein (größer als 0): ");
+                    scanf("%d", &n_process); // Anzahl der zu erstellenden Prozesse auslesen
 
                     if (n_process <= 0) {
-                        printf("Invalid number of processes.\n");
+                        printf("Ungültige Anzahl von Prozessen.\n"); // Fehlermeldung, falls ungültige Anzahl
                         n_process = 0;
                     } else {
-                        printf("Creating %d processes...\n", n_process);
-                        printf("Parent Process (PID: %d) forks into:\n", getpid());
-                        fprintf(fpo, "Parent Process (PID: %d) forks into:\n", getpid());
+                        printf("%d Prozess(e) wird/werden erstellt...\n", n_process);
+                        printf("Elternprozess (PID: %d) verzweigt in:\n", getpid()); // PID des Elternprozesses ausgeben
+                        fprintf(fpo, "Elternprozess (PID: %d) verzweigt in:\n", getpid()); // PID des Elternprozesses in Ausgabedatei schreiben
                         for (i = 0; i < n_process; i++) {
-                            ret = do_fork();
+                            ret = do_fork(); // Kindprozess erzeugen
                             if (ret <= 0) {
-                                printf("Failed to create process %d.\n", i + 1);
+                                printf("Fehler beim Erstellen des %d. Prozesses.\n", i + 1); // Fehlermeldung, falls Erzeugung fehlschlägt
                                 exit(ret);
                             }
                             if (ret > 0) {
-                                printf("  |- Child Process %d (PID: %d)\n", i + 1, ret);
-                                fprintf(fpo, "Child Process %d (PID: %d)\n", i + 1, ret);
+                                printf("  |- Kindprozess %d (PID: %d)\n", i + 1, ret); // PID des Kindprozesses ausgeben
+                                fprintf(fpo, "Kindprozess %d (PID: %d)\n", i + 1, ret); // PID des Kindprozesses in Ausgabedatei schreiben
                             }
-                            sleep(2);
+                            sleep(2); // Kurze Verzögerung zwischen den Kindprozessen
                         }
-                        printf("Processes created successfully.\n");
+                        printf("Prozesse erfolgreich erstellt.\n");
                     }
                 }
                 break;
             }
             case 2: {
-                // Read project_out.txt
-                printFileContents(output);
+                // project_out.txt lesen
+                printFileContents(output); // Dateiinhalt ausgeben
                 break;
             }
             case 3: {
-                // Exit
+                // Beenden
                 if (n_process > 0) {
-                    killAllChildProcesses();
+                    killAllChildProcesses(); // Alle Kindprozesse beenden
                 }
-                printf("Exiting the program.\n");
-                fclose(fpo);
-                exit(0);
+                printf("Programm wird beendet.\n");
+                fclose(fpo); // Ausgabedatei schließen
+                exit(0); // Programm beenden
                 break;
             }
             default:
-                printf("Invalid choice. Please try again.\n");
+                printf("Ungültige Auswahl. Bitte versuchen Sie es erneut.\n"); // Fehlermeldung, falls ungültige Auswahl
         }
     }
 }
